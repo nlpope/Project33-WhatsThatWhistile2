fd3838f989c8972ce43f162ca74484687b4c5340
Noah Pope <npope@tutanota.com>
2026-01-16 10:02:23 -0600

    creating AddCommentsvc & Submitvc

---
+++ Project33-WhatsThatWhistile2/Screens/SubmitVC.swift
@@ -0,0 +1,26 @@
+//  File: SubmitVC.swift
+//  Project: Project33-WhatsThatWhistile2
+//  Created by: Noah Pope on 1/16/26.
+
+import UIKit
+
+class SubmitVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
