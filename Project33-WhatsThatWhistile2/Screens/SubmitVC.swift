51ffc91688c7e70a91b3ea162120021d4f4e2d5a
Noah Pope <npope@tutanota.com>
2026-01-16 11:58:19 -0600

    adding status label & spinner to SubmitVC

--- Project33-WhatsThatWhistile2/Screens/SubmitVC.swift
+++ Project33-WhatsThatWhistile2/Screens/SubmitVC.swift
@@ -9,13 +9,15 @@ class SubmitVC: UIViewController
     var genre: String!
     var comments: String!
     var stackView: UIStackView!
-    var status: UILabel!
+    var statusLabel: UILabel!
     var spinner: UIActivityIndicatorView!
     
     override func loadView()
     {
         configView()
         configStackView()
+        configStatusLabel()
+        configSpinner()
     }
     
     
@@ -36,6 +38,44 @@ class SubmitVC: UIViewController
     
     func configStackView()
     {
+        stackView = UIStackView()
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = UIStackView.Distribution.fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
         
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    
+    func configStatusLabel()
+    {
+        statusLabel = UILabel()
+        statusLabel.translatesAutoresizingMaskIntoConstraints = false
+        statusLabel.text = "Submitting..."
+        statusLabel.textColor = UIColor.white
+        statusLabel.font = UIFont.preferredFont(forTextStyle: .title1)
+        statusLabel.numberOfLines = 0
+        statusLabel.textAlignment = .center
+        
+        stackView.addArrangedSubview(statusLabel)
+    }
+    
+    
+    func configSpinner()
+    {
+        spinner = UIActivityIndicatorView(style: .whiteLarge)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        spinner.startAnimating()
+        
+        stackView.addArrangedSubview(spinner)
     }
 }
